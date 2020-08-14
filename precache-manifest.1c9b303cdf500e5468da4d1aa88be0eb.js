7bf8058b3f27339302cf8752ae128598033e71ba
madhanJayaprakash <madhan.it86@gmail.com>
2020-08-14 23:39:39 +0530

    Updates

---
+++ precache-manifest.1c9b303cdf500e5468da4d1aa88be0eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b6a91a686dd19600adbfbbee4154c1",
+    "url": "/weKan/index.html"
+  },
+  {
+    "revision": "fdf231d97c984d207ad7",
+    "url": "/weKan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38c9587f376c6594c55f",
+    "url": "/weKan/static/js/2.fac21d66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weKan/static/js/2.fac21d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf231d97c984d207ad7",
+    "url": "/weKan/static/js/main.83ae4ad2.chunk.js"
+  },
+  {
+    "revision": "542b3ccb924cd2ade06d",
+    "url": "/weKan/static/js/runtime-main.368f8299.js"
+  }
+]);
